82300e064496342788df83d26fd3ce58d07fab84
jpylypiw <12394156+jpylypiw@users.noreply.github.com>
2019-04-12 20:01:03 +0200

    puppet 6 compatibility

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,7 +2,7 @@
 
 All notable changes to this project will be documented in this file.
 
-## Release 0.1.0
+## Release 0.1.1
 
 **Features**
 - First implementation
@@ -11,13 +11,17 @@ All notable changes to this project will be documented in this file.
 
 **Known Issues**
 - Only Linux (Debian, CentOS, SuSE Ubuntu) supported
+- Only tested on RedHat7
 
-## Release 0.2.0
+## Release 0.1.2
 
 **Features**
-- Changed travis settings for a better testing performance
-- Changed README.md for proper documentation
+- Added requriements for puppet6
+- updates modules dependencies and fixtures
+- updates travis and gitlab ci
+- added test scripts
 
 **Bugfixes**
 
 **Known Issues**
+- Only Linux (Debian, CentOS, SuSE Ubuntu) supported
--- manifests/repo.pp
+++ manifests/repo.pp
@@ -1,73 +1,57 @@
 # heartbeat::repo
 # @api private
 #
-# @summary It manages the package repositories to install heartbeat
+# @summary It manages the package repositories to isntall heartbeat
 class heartbeat::repo {
-  case $heartbeat::manage_repo {
-    true: {
-      $repo_ensure = $heartbeat::ensure
-    }
-    default: {
-      $repo_ensure = 'absent'
-    }
-  }
-
-  case $facts['osfamily'] {
-    'Debian': {
-      include ::apt
-
-      $download_url = 'https://artifacts.elastic.co/packages/6.x/apt'
-
-      if !defined(Apt::Source['beats']) {
-        apt::source{'beats':
-          ensure   => $repo_ensure,
-          location => $download_url,
-          release  => 'stable',
-          repos    => 'main',
-          key      => {
-            id     => '46095ACC8548582C1A2699A9D27D666CD88E42B4',
-            source => 'https://artifacts.elastic.co/GPG-KEY-elasticsearch',
-          },
+  if ($heartbeat::manage_repo == true) and ($heartbeat::ensure == 'present') {
+    case $facts['osfamily'] {
+      'Debian': {
+        include ::apt
+        if !defined(Apt::Source['beats']) {
+          apt::source{'beats':
+            ensure   => $heartbeat::ensure,
+            location => 'https://artifacts.elastic.co/packages/6.x/apt',
+            release  => 'stable',
+            repos    => 'main',
+            key      => {
+              id     => '46095ACC8548582C1A2699A9D27D666CD88E42B4',
+              source => 'https://artifacts.elastic.co/GPG-KEY-elasticsearch',
+            },
+          }
         }
       }
-    }
-    'RedHat': {
-
-      $download_url = 'https://artifacts.elastic.co/packages/6.x/yum'
-
-      if !defined(Yumrepo['beats']) {
-        yumrepo{'beats':
-          ensure   => $repo_ensure,
-          descr    => 'Elastic repository for 6.x packages',
-          baseurl  => $download_url,
-          gpgcheck => 1,
-          gpgkey   => 'https://artifacts.elastic.co/GPG-KEY-elasticsearch',
-          enabled  => 1,
+      'RedHat': {
+        if !defined(Yumrepo['beats']) {
+          yumrepo{'beats':
+            ensure   => $heartbeat::ensure,
+            descr    => 'Elastic repository for 6.x packages',
+            baseurl  => 'https://artifacts.elastic.co/packages/6.x/yum',
+            gpgcheck => 1,
+            gpgkey   => 'https://artifacts.elastic.co/GPG-KEY-elasticsearch',
+            enabled  => 1,
+          }
         }
       }
-    }
-    'SuSe': {
-
-      $download_url = 'https://artifacts.elastic.co/packages/6.x/yum'
-
-      exec { 'topbeat_suse_import_gpg':
-        command => '/usr/bin/rpmkeys --import https://artifacts.elastic.co/GPG-KEY-elasticsearch',
-        unless  => '/usr/bin/test $(rpm -qa gpg-pubkey | grep -i "D88E42B4" | wc -l) -eq 1 ',
-        notify  => [ Zypprepo['beats'] ],
-      }
-      if !defined (Zypprepo['beats']) {
-        zypprepo{'beats':
-          baseurl     => $download_url,
-          enabled     => 1,
-          autorefresh => 1,
-          name        => 'beats',
-          gpgcheck    => 1,
-          gpgkey      => 'https://artifacts.elastic.co/GPG-KEY-elasticsearch',
-          type        => 'yum',
+      'SuSe': {
+        exec { 'topbeat_suse_import_gpg':
+          command => '/usr/bin/rpmkeys --import https://artifacts.elastic.co/GPG-KEY-elasticsearch',
+          unless  => '/usr/bin/test $(rpm -qa gpg-pubkey | grep -i "D88E42B4" | wc -l) -eq 1 ',
+          notify  => [ Zypprepo['beats'] ],
+        }
+        if !defined (Zypprepo['beats']) {
+          zypprepo{'beats':
+            baseurl     => 'https://artifacts.elastic.co/packages/6.x/yum',
+            enabled     => 1,
+            autorefresh => 1,
+            name        => 'beats',
+            gpgcheck    => 1,
+            gpgkey      => 'https://artifacts.elastic.co/GPG-KEY-elasticsearch',
+            type        => 'yum',
+          }
         }
       }
-    }
-    default: {
+      default: {
+      }
     }
   }
 }
